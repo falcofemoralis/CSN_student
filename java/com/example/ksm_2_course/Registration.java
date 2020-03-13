de43c1cf030f248b996b0f65fcdc9da5ed1077f4
Illia <61616419+Derlados@users.noreply.github.com>
2020-03-13 18:28:56 +0200

    Update data_disc.json
    Update Registration.java

--- java/com/example/ksm_2_course/Registration.java
+++ java/com/example/ksm_2_course/Registration.java
@@ -21,19 +21,25 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 public class Registration extends AppCompatActivity {
 
+    final String MAIN_URL = "http://192.168.0.105/registr/Rating/", FILE_NAME = "data_disc.json";
     EditText password, checkPassword, nickName;
     String group;
     Button registration;
     RequestQueue requestQueue;
-    String url = "http://192.168.0.105/registr/Rating/registration.php";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -69,23 +75,91 @@ public class Registration extends AppCompatActivity {
         overridePendingTransition(0, 0);
     }
 
-    public void OnClick(View view)
-    {
+    public void registrtion(View view) {
+        String url = MAIN_URL + "registration.php";
         String name = nickName.getText().toString();
-        if(name.equals("")){
+        if (name.equals("")) {
             Toast.makeText(Registration.this, "Please enter nickname", Toast.LENGTH_SHORT).show();
             return;
         }
 
         String pass = password.getText().toString();
         String checkpass = checkPassword.getText().toString();
-        if (!pass.equals(checkpass)){
+        if (!pass.equals(checkpass)) {
             Toast.makeText(Registration.this, "Incorrect password", Toast.LENGTH_SHORT).show();
             return;
-        }else if(pass.equals("") || checkpass.equals("")) {
+        } else if (pass.equals("") || checkpass.equals("")) {
             Toast.makeText(Registration.this, "Enter password", Toast.LENGTH_SHORT).show();
             return;
         }
+
+        requestQueue = Volley.newRequestQueue(getApplicationContext());
+        StringRequest request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                if (response.indexOf("Duplicate") != -1)
+                    Toast.makeText(Registration.this, "This nickname is taken by another user", Toast.LENGTH_SHORT).show();
+                else {
+                    Toast.makeText(Registration.this, "Successfully registration", Toast.LENGTH_SHORT).show();
+
+                    Gson gson = new Gson();
+                    ArrayList<Discipline> discs = new ArrayList<Discipline>();
+                    Type listType = new TypeToken<List<Discipline>>() {
+                    }.getType();
+                    discs = gson.fromJson(JSONHelper.read(Registration.this, FILE_NAME), listType);
+
+                    for (int i = 0; i < discs.size(); ++i)
+                    {
+                        Discipline temp = discs.get(i);
+                        setEmpryRating(temp.getName(), gson.toJson(temp.getComplete()));
+                    }
+
+                    Save();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(Registration.this, "No connection", Toast.LENGTH_SHORT).show();
+            }
+        }) {
+
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> parameters = new HashMap<String, String>();
+                parameters.put("NickName", nickName.getText().toString().toLowerCase());
+                parameters.put("Password", password.getText().toString());
+                parameters.put("NameGroup", group);
+                return parameters;
+            }
+        };
+        requestQueue.add(request);
+
+
+    }
+    public void Save()
+    {
+        SharedPreferences.Editor pref = PreferenceManager.getDefaultSharedPreferences(this).edit();
+        pref.putBoolean(SettingsActivity.KEY_IS_REGISTERED,false);
+        pref.putString(SettingsActivity.KEY_NICKNAME,nickName.getText().toString());
+        pref.putString(SettingsActivity.KEY_PASSWORD,password.getText().toString());
+        pref.putString(SettingsActivity.KEY_GROUP,group);
+        pref.apply();
+
+        Intent intent;
+        intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onBackPressed() {
+        moveTaskToBack(true);
+    }
+
+    protected void setEmpryRating(final String NameDiscp, final String status)
+    {
+        String url = MAIN_URL + "insertRating.php";
 
         requestQueue = Volley.newRequestQueue(getApplicationContext());
         StringRequest request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>()
@@ -112,32 +186,13 @@ public class Registration extends AppCompatActivity {
             protected Map<String, String> getParams() throws AuthFailureError {
                 Map<String, String> parameters = new HashMap<String, String>();
                 parameters.put("NickName", nickName.getText().toString().toLowerCase());
-                parameters.put("Password", password.getText().toString());
-                parameters.put("NameGroup", group);
+                parameters.put("NameDiscp", NameDiscp);
+                parameters.put("Status", status);
                 return parameters;
             }
         };
         requestQueue.add(request);
     }
 
-    public void Save()
-    {
-        SharedPreferences.Editor pref = PreferenceManager.getDefaultSharedPreferences(this).edit();
-        pref.putBoolean(SettingsActivity.KEY_IS_REGISTERED,false);
-        pref.putString(SettingsActivity.KEY_NICKNAME,nickName.getText().toString());
-        pref.putString(SettingsActivity.KEY_PASSWORD,password.getText().toString());
-        pref.putString(SettingsActivity.KEY_GROUP,group);
-        pref.apply();
-
-        Intent intent;
-        intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-    }
-
-    @Override
-    public void onBackPressed() {
-        moveTaskToBack(true);
-    }
-
-
 }
+

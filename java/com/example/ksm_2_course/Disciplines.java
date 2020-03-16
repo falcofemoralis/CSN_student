b7b1c96f093cb2b8a2ec45d347f3ce03bcaad530
Illia <61616419+Derlados@users.noreply.github.com>
2020-03-16 14:50:53 +0200

    Update activity_disciplines.xml
    Update Disciplines.java

--- java/com/example/ksm_2_course/Disciplines.java
+++ java/com/example/ksm_2_course/Disciplines.java
@@ -1,29 +1,45 @@
 package com.example.ksm_2_course;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceManager;
+
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Disciplines extends AppCompatActivity
 {
     final String FILE_NAME = "data_disc.json"; 
     final int FALSE = 0xFFF56D6D, TRUE = 0xFFDFFFBF; // FALSE(Не сдано) - красный, TRUE(Сдано) - светозеленый
 
+    RequestQueue requestQueue;
     Button res; // Кнопка результата
     Button buts[][] = new Button[7][2]; // Кнопки "Сдано" и "Защита"
     int complete = 0, Labs; // complete - подсчет сданих лаб, Labs - хранит количество лабораторних
     ArrayList<Discipline> discs = new ArrayList<Discipline>(); //Дисциплины
     Discipline current; // текущая дисциплина
+    SharedPreferences pref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -32,6 +48,8 @@ public class Disciplines extends AppCompatActivity
         setContentView(R.layout.activity_disciplines);
         Intent intent = getIntent();
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        pref = PreferenceManager.getDefaultSharedPreferences(Disciplines.this);
 
         // Достать объект Дисциплина с json, возвращает массив дисциплин
         int num = GetCode(intent.getStringExtra("Name")); // индекс для выбора дисциплины из массива дисциплин
@@ -100,7 +118,7 @@ public class Disciplines extends AppCompatActivity
         res.setText(Integer.toString(complete * 50 / Labs) + "%"); // Установка среднего прогресса по дисциплине
     }
 
-    protected void SaveAll()
+    public void SaveAll(View v)
     {
         boolean[][] compl_but = new boolean[Labs][2];
 
@@ -126,6 +144,8 @@ public class Disciplines extends AppCompatActivity
         Gson gson = new Gson();
         String jsonString = gson.toJson(discs);
         JSONHelper.create(this, FILE_NAME, jsonString);
+
+        updateRating(pref.getString(SettingsActivity.KEY_NICKNAME, ""), current.getName(), gson.toJson(compl_but));
     }
     
     //Смена статуса полей Сдано и Защита
@@ -145,7 +165,6 @@ public class Disciplines extends AppCompatActivity
         }
 
         res.setText(Integer.toString(complete * 50 / Labs) + "%"); // Установка поля среднего прогресса по дисциплине
-         SaveAll(); // Сохраняем изменения
     }
 
     // Функция получения кода текущей дисциплины
@@ -170,6 +189,36 @@ public class Disciplines extends AppCompatActivity
         }
     }
 
-}
+    protected void updateRating( final String NickName, final String NameDiscp, final String status)
+    {
+        String url = MainActivity.MAIN_URL + "updateRating.php";
 
+        requestQueue = Volley.newRequestQueue(getApplicationContext());
+        StringRequest request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>()
+        {
+            @Override
+            public void onResponse(String response)
+            {
+                Toast.makeText(Disciplines.this, "data saved successfully", Toast.LENGTH_SHORT).show();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error)
+            {
+                Toast.makeText(Disciplines.this, "No connection with server, data saved locally", Toast.LENGTH_LONG).show();
+            }
+        }) {
+
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> parameters = new HashMap<String, String>();
+                parameters.put("NickName", NickName);
+                parameters.put("NameDiscp", NameDiscp);
+                parameters.put("Status", status);
+                return parameters;
+            }
+        };
+        requestQueue.add(request);
+    }
+}
 

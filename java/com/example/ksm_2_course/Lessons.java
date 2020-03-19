94a2d8c9bee2f377aa37026ae0e2af276c51b1a7
Illia <61616419+Derlados@users.noreply.github.com>
2020-03-19 19:00:00 +0200

    Update AndroidManifest.xml
    Add files via upload
    
    Add files via upload
    
    Update activity_lessons_schedule.xml
    
    Update knt518.json
    
    Update knt528.json
    
    Update Lessons.java

--- java/com/example/ksm_2_course/Lessons.java
+++ java/com/example/ksm_2_course/Lessons.java
@@ -2,231 +2,134 @@ package com.example.ksm_2_course;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.preference.PreferenceManager;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TableLayout;
 import android.widget.TextView;
 import com.google.gson.Gson;
+
+import java.util.ArrayList;
 import java.util.Calendar;
 import com.google.gson.reflect.TypeToken;
 import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.List;
 
-public class Lessons extends AppCompatActivity {
+public class Lessons extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
-    TableLayout tableLayout_r[] = new TableLayout[4];
-    TextView textViewMonday[][] = new TextView[4][4];
-    TextView textViewTuesday[][] = new TextView[4][4];
-    TextView textViewWednesday[][] = new TextView[4][4];
-    TextView textViewThursday[][] = new TextView[4][4];
-    static  String FILE_NAME;
-    int first_day=2 ;
-    static int week;
-    Calendar calendar = Calendar.getInstance();
+    final int MAX_PAIR = 5;
+    final int MAX_DAYS = 5;
+
+    Lesson[] lessons = new Lesson[2];
+    TextView[][] schedule = new TextView[MAX_PAIR][MAX_DAYS];
+    TextView type_week;
+    Spinner group_spin;
+    String  file_name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lessons_schedule);
-        setBorders();
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 
-        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
-        String text = pref.getString(SettingsActivity.KEY_GROUP,"knt_528");
-        //выбираю базу данных
-        if (text.equals("КНТ-518")) {
-            FILE_NAME = "knt518.json";
-        } else if (text.equals("КНТ-528")) {
-            FILE_NAME = "knt528.json";
-        }
-
-        //создаю обьекты таблицы
-        int id;
-
-        String table1 = "table", stringId;
-        for (int i = 0; i < 4; i++) {
-            for (int j = 1; j < 4; j++) {
-                stringId = table1 + Integer.toString(i) + "_" + Integer.toString(j);
-                id = getResources().getIdentifier(stringId, "id", getApplicationContext().getPackageName());
-                textViewMonday[i][j] = (TextView) findViewById(id);
-            }
-        }
-
-        String table2 = "table2", stringId2;
-        for (int i = 0; i < 4; i++) {
-            for (int j = 1; j < 4; j++) {
-                stringId2 = table2 + Integer.toString(i) + "_" + Integer.toString(j);
-                id = getResources().getIdentifier(stringId2, "id", getApplicationContext().getPackageName());
-                textViewTuesday[i][j] = (TextView) findViewById(id);
-            }
-        }
-
-        String table3 = "table3", stringId3;
-        for (int i = 0; i < 4; i++) {
-            for (int j = 1; j < 4; j++) {
-                stringId3 = table3 + Integer.toString(i) + "_" + Integer.toString(j);
-                id = getResources().getIdentifier(stringId3, "id", getApplicationContext().getPackageName());
-                textViewWednesday[i][j] = (TextView) findViewById(id);
-            }
-        }
-
-        String table4 = "table4", stringId4;
-        for (int i = 0; i < 4; i++) {
-            for (int j = 1; j < 4; j++) {
-                stringId4 = table4 + Integer.toString(i) + "_" + Integer.toString(j);
-                id = getResources().getIdentifier(stringId4, "id", getApplicationContext().getPackageName());
-                textViewThursday[i][j] = (TextView) findViewById(id);
-            }
-        }
-
-
-        //узнаю тип недели (числитель или знаменатель)
-        int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
-        TextView nameOfWeek = (TextView) findViewById(R.id.nameOfWeek);
-        Button changeScheduleButton = (Button) findViewById(R.id.changeScheduleButton);
-        if (dayOfMonth == 1) {
-            week = 1;
-            nameOfWeek.setText("ЧИСЕЛЬНИК");
-            changeScheduleButton.setText("ЗНАМЕННИК");
-        } else if (((dayOfMonth - first_day) / 7) % 2 == 0) {
-            week = 0;
-            nameOfWeek.setText("ЗНАМЕННИК");
-            changeScheduleButton.setText("ЧИСЕЛЬНИК");
-        } else {
-            week = 1;
-            nameOfWeek.setText("ЧИСЕЛЬНИК");
-            changeScheduleButton.setText("ЗНАМЕННИК");
-        }
-
-        setDataBase();
-
+        createSpinner();
+        setSchedule();
+        updateSchedule();
     }
 
-    public void setBorders(){
-        //получаем день недели (пн,вт и пр)
-        int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
-
-        //создаем обьект для обводки
-        tableLayout_r[0] = (TableLayout) findViewById(R.id.tableLayout_mon);
-        tableLayout_r[1] = (TableLayout) findViewById(R.id.tableLayout_tue);
-        tableLayout_r[2] = (TableLayout) findViewById(R.id.tableLayout_wed);
-        tableLayout_r[3] = (TableLayout) findViewById(R.id.tableLayout_thur);
-
-        //при совпадении дней делаем обводку красной (т.е меняем файл с noborders на borders)
-        if (dayOfWeek == 2) {
-            tableLayout_r[0].setBackground(getResources().getDrawable(R.drawable.borders));
-        } else if (dayOfWeek == 3) {
-            tableLayout_r[1].setBackground(getResources().getDrawable(R.drawable.borders));
-        } else if (dayOfWeek == 4) {
-            tableLayout_r[2].setBackground(getResources().getDrawable(R.drawable.borders));
-        } else if (dayOfWeek == 5) {
-            tableLayout_r[3].setBackground(getResources().getDrawable(R.drawable.borders));
-        }
-    }
-
-    public void OnClick(View v)
+    protected void updateSchedule()
     {
-        TextView nameOfWeek = (TextView) findViewById(R.id.nameOfWeek);
-        Button changeScheduleButton = (Button) findViewById(R.id.changeScheduleButton);
-       if(week == 1) {
-           nameOfWeek.setText("ЗНАМЕННИК");
-           changeScheduleButton.setText("ЧИСЕЛЬНИК");
-           week = 0;
-       } else if(week == 0) {
-           nameOfWeek.setText("ЧИСЕЛЬНИК");
-           changeScheduleButton.setText("ЗНАМЕННИК");
-           week = 1;
-       }
-       setDataBase();
+        int numType = type_week.getText().equals("Знаменник") ? 0 : 1;
+        String cell;
+
+        if((group_spin.getSelectedItem().toString()).equals("КНТ-518"))
+            file_name = "knt518.json";
+        else
+            file_name = "knt528.json";
+
+        Gson g = new Gson();
+        try {
+            lessons = g.fromJson(JSONHelper.loadJSONFromAsset(this, file_name), Lesson[].class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        for (int i = 0, size = lessons[numType].types.size() / 4; i < size; ++i)
+        {
+            for (int j = 0; j < 4; ++j)
+            {
+                ItemType item = lessons[numType].types.get(i * 4 + j);
+
+                if (item.getSubject().equals("-"))
+                {
+                    schedule[j][i].setText("");
+                    continue;
+                }
+                cell = item.getSubject() + " " + item.getType() + " (" + item.getRoom() + ")";
+                schedule[j][i].setText(cell);
+            }
+        }
     }
 
-    public void setDataBase(){
-        //открываю файл и загружаю данные с базы
-        try{
+    protected void setSchedule()
+    {
+        String stringId, table;
+        int id;
+        type_week = findViewById(R.id.type_week);
 
-            //gson
-            Gson gson = new Gson();
-            Type listType = new TypeToken<List<Lesson>>() {}.getType();
-            List<Lesson> list = gson.fromJson(JSONHelper.loadJSONFromAsset(this,FILE_NAME), listType);
+        for (int i = 0; i < MAX_PAIR; ++i) {
+            table = "text" + (i + 1) +  "_";
 
-            //выбираем неделю
-            Lesson day = list.get(week);
-
-            //выбираем номер пары в неделе
-            int count = 0,j=1;
-            for(int i=0;i<4;i++){
-                ItemType lesson = day.types.get(count);
-                textViewMonday[i][j].setText(lesson.getRoom()); j++;
-                textViewMonday[i][j].setText(lesson.getSubject()); j++;
-                textViewMonday[i][j].setText(lesson.getType());
-                if(lesson.getType().equals("ЛК")){
-                    textViewMonday[i][j].setBackgroundColor(getResources().getColor(R.color.green));
-                }else if(lesson.getType().equals("ЛР")){
-                    textViewMonday[i][j].setBackgroundColor(getResources().getColor(R.color.orange));
-                }else if(lesson.getType().equals("-")){
-                    textViewMonday[i][j].setBackgroundColor(getResources().getColor(R.color.blue));
-                }
-                j=1;
-                count++;
+            for (int j = 0; j < MAX_DAYS; ++j) {
+                stringId = table + (j + 2);
+                id = getResources().getIdentifier(stringId, "id", getApplicationContext().getPackageName());
+                schedule[i][j] = findViewById(id);
             }
-
-            for(int i=0;i<4;i++){
-                ItemType lesson = day.types.get(count);
-                textViewTuesday[i][j].setText(lesson.getRoom()); j++;
-                textViewTuesday[i][j].setText(lesson.getSubject()); j++;
-                textViewTuesday[i][j].setText(lesson.getType());
-                if(lesson.getType().equals("ЛК")){
-                    textViewTuesday[i][j].setBackgroundColor(getResources().getColor(R.color.green));
-                }else if(lesson.getType().equals("ЛР")){
-                    textViewTuesday[i][j].setBackgroundColor(getResources().getColor(R.color.orange));
-                }else if(lesson.getType().equals("-")){
-                    textViewTuesday[i][j].setBackgroundColor(getResources().getColor(R.color.blue));
-                }
-                j=1;
-                count++;
-            }
-
-            for(int i=0;i<4;i++){
-                ItemType lesson = day.types.get(count);
-                textViewWednesday[i][j].setText(lesson.getRoom()); j++;
-                textViewWednesday[i][j].setText(lesson.getSubject()); j++;
-                textViewWednesday[i][j].setText(lesson.getType());
-                if(lesson.getType().equals("ЛК")){
-                    textViewWednesday[i][j].setBackgroundColor(getResources().getColor(R.color.green));
-                }else if(lesson.getType().equals("ЛР")){
-                    textViewWednesday[i][j].setBackgroundColor(getResources().getColor(R.color.orange));
-                }else if(lesson.getType().equals("-")){
-                    textViewWednesday[i][j].setBackgroundColor(getResources().getColor(R.color.blue));
-                }
-                j=1;
-                count++;
-            }
-
-            for(int i=0;i<4;i++){
-                ItemType lesson = day.types.get(count);
-                textViewThursday[i][j].setText(lesson.getRoom()); j++;
-                textViewThursday[i][j].setText(lesson.getSubject()); j++;
-                textViewThursday[i][j].setText(lesson.getType());
-                if(lesson.getType().equals("ЛК")){
-                    textViewThursday[i][j].setBackgroundColor(getResources().getColor(R.color.green));
-                }else if(lesson.getType().equals("ЛР")){
-                    textViewThursday[i][j].setBackgroundColor(getResources().getColor(R.color.orange));
-                }else if(lesson.getType().equals("-")){
-                    textViewThursday[i][j].setBackgroundColor(getResources().getColor(R.color.blue));
-                }
-                j=1;
-                count++;
-            }
-
         }
-        catch (IOException ex){
-            ex.printStackTrace();
-        }
+        updateSchedule();
+    }
+
+    public void changeTypeWeek(View v)
+    {
+        if (type_week.getText().equals("Знаменник"))
+            type_week.setText("Чисельник");
+        else
+            type_week.setText("Знаменник");
+        updateSchedule();
+    }
+
+    protected  void createSpinner()
+    {
+        group_spin = findViewById(R.id.group_spin);
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.group_values,
+                R.layout.color_spinner_schedule
+        );
+
+        adapter.setDropDownViewResource(R.layout.spinner_dropdown_schedule);
+        group_spin.setAdapter(adapter);
+        group_spin.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
+    {
+        updateSchedule();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
+
 
 

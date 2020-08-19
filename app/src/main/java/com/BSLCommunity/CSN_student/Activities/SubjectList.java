bd77db0daa87fbaca18de5184a25cb8441f8f538
Illia <derladoshome@gmail.com>
2020-08-19 23:06:15 +0300

    Added the ability to download an image from the Internet

--- app/src/main/java/com/BSLCommunity/CSN_student/Activities/SubjectList.java
+++ app/src/main/java/com/BSLCommunity/CSN_student/Activities/SubjectList.java
@@ -1,11 +1,15 @@
 package com.BSLCommunity.CSN_student.Activities;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Point;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
@@ -14,6 +18,7 @@ import android.widget.Space;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
@@ -21,6 +26,11 @@ import androidx.core.content.ContextCompat;
 import com.BSLCommunity.CSN_student.Objects.Subjects;
 import com.BSLCommunity.CSN_student.Objects.SubjectsInfo;
 import com.BSLCommunity.CSN_student.R;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageRequest;
+import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -43,6 +53,7 @@ public class SubjectList extends AppCompatActivity {
         }
     }
 
+    BitmapDrawable img;
     TableLayout tableSubjects; // Лаяут всех дисциплин
     int[] progresses; // Прогресс для каждого предмета
 
@@ -53,6 +64,7 @@ public class SubjectList extends AppCompatActivity {
 
         tableSubjects = findViewById(R.id.activity_subject_list_ll_table_subjects);
 
+        getImageFromServer();
         //получаем список групп
         Subjects.getSubjectsList(this, new Callable<Void>() {
             @Override
@@ -62,6 +74,26 @@ public class SubjectList extends AppCompatActivity {
                 return null;
             }
         });
+    }
+
+    protected void getImageFromServer() {
+
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+
+        ImageRequest imageRequest = new ImageRequest("http://192.168.1.3", new Response.Listener<Bitmap>() {
+
+            @Override
+            public void onResponse(Bitmap response) {
+                img = new BitmapDrawable(response);
+            }
+        }, 0, 0, null, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), error.toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        requestQueue.add(imageRequest);
     }
 
     @Override
@@ -77,7 +109,7 @@ public class SubjectList extends AppCompatActivity {
         super.onPause();
     }
 
-    // Создаем дисциплину
+    // Создаем кнопку одной дисциплины
     protected void createSubject(TableRow rowSubject, int numberSubject) {
 
         // Создание лаяута для кнопки и текстовых полей о статистике по предмету
@@ -98,6 +130,19 @@ public class SubjectList extends AppCompatActivity {
         } catch (JSONException e) { }
         subjectBt.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 12); // Устанавливаем размер текста
         subjectBt.setMaxEms(1); // Для того чтобы текст не растягивал кнопки
+        subjectBt.setGravity(Gravity.BOTTOM);
+        subjectBt.setGravity(Gravity.CENTER);
+
+        // Устанавливаем изображение дисциплины
+        WindowManager w = getWindowManager();
+        Point size = new Point();
+        w.getDefaultDisplay().getSize(size);
+
+        int sizeIm = (int)(size.x * 0.5 / 4.2);
+
+        img.setBounds( 0, 0, sizeIm, sizeIm);
+        subjectBt.setCompoundDrawables(null, img, null, null);
+
         subjectBt.setBackgroundResource(R.drawable.ic_subject_list_v2); // Устанавливаем фон для кнопки
 
         final int subjectId = IdGenerator.getId();
@@ -122,8 +167,8 @@ public class SubjectList extends AppCompatActivity {
         progressText.setLayoutParams(paramsText); // Устанавливаем параметры макета
         progressText.setGravity(Gravity.CENTER); // Устанавливаем позицию текста в центре кнопки
         progressText.setTextColor( ContextCompat.getColor(this, R.color.white)); // Устанавливаем цвет текста
-        progressText.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 16);  // Устанавливаем размер текста
-        progressText.setText(progresses[numberSubject] + "%");
+        progressText.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 12);  // Устанавливаем размер текста
+        progressText.setText(progresses[numberSubject] + " %");
 
         subjectLayout.addView(progressText); // Добавляем текст
 
@@ -147,15 +192,12 @@ public class SubjectList extends AppCompatActivity {
         subjectBt.setMaxEms(1); // Для того чтобы текст не растягивал кнопки
         subjectBt.setBackgroundResource(R.drawable.ic_subject_list_v2); // Устанавливаем фон для кнопки
 
-        final int subjectId = IdGenerator.getId();
+
         View.OnClickListener onClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Animation click = AnimationUtils.loadAnimation(SubjectList.this, R.anim.btn_click);
-                view.startAnimation(click);
-                Intent intent = new Intent(SubjectList.this, SubjectInfo.class);
-                intent.putExtra("button_id", subjectId);
-                startActivity(intent);
+                // Здесь должен быть переход на активити полного рейтинга
+                return;
             }
         };
         subjectBt.setOnClickListener(onClickListener); // Добавляем функционал кнопке
@@ -176,6 +218,7 @@ public class SubjectList extends AppCompatActivity {
         for (i = 0; i <= Subjects.subjectsList.length; ++i) {
             // В одном ряду может быть лишь 3 кнопки, если уже три созданы, создается следующая колонка
             if (i % 3 == 0) {
+
                 // Добавляем последний разделитель между краем экрана и крайней правой кнопкой
                 if (rowSubject != null) {
                     // Создаем разделители
@@ -183,7 +226,6 @@ public class SubjectList extends AppCompatActivity {
                     tableSubjects.addView(new Space(this), new TableLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT, 0.3f));
                 }
 
-                // Создаем новый пустой ряд для кнопок
                 ViewGroup.LayoutParams params = new TableLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT, 2f);
                 rowSubject = new TableRow(this);
                 rowSubject.setOrientation(TableRow.HORIZONTAL);
@@ -214,7 +256,6 @@ public class SubjectList extends AppCompatActivity {
         rowSubject.addView(new Space(this), new TableRow.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, 0.3f));
     }
 
-
     //устанавливаем прогресс внизу экрана
     public void setProgress() {
         SubjectsInfo subjectsInfo = SubjectsInfo.getInstance(this);
@@ -241,7 +282,7 @@ public class SubjectList extends AppCompatActivity {
             subjectAllWork = (subjectsInfo.subjectInfo[i].labsCount + subjectsInfo.subjectInfo[i].ihwCount + subjectsInfo.subjectInfo[i].otherCount);
 
             // Считаем процент выполненной работы студент за дисциплину
-            progresses[i] = subjectAllWork * 100 / (subjectComplete);
+            progresses[i] = subjectComplete != 0 ? subjectComplete * 100 / (subjectAllWork) : 0;
             sumProgress += progresses[i];
         }
 

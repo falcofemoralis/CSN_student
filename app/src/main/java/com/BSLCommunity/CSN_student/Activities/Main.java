9148efe3c784484d3c8a118bf46c27942fd63bcd
Falco Femoralis <araraunapappagallo@gmail.com>
2020-08-27 23:13:48 +0300

    New main menu

--- app/src/main/java/com/BSLCommunity/CSN_student/Activities/Main.java
+++ app/src/main/java/com/BSLCommunity/CSN_student/Activities/Main.java
@@ -4,10 +4,12 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.graphics.Color;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.TextView;
@@ -91,8 +93,6 @@ public class Main extends AppCompatActivity {
                 return null;
             }
         });
-
-
     }
 
     public void OnClick(View view) {

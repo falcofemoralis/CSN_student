4330e89c55bd591fa7c5ee2fb97404904f9ca7d2
Illia <derladoshome@gmail.com>
2020-09-04 21:26:52 +0300

    drawed user list

--- app/src/main/java/com/BSLCommunity/CSN_student/Activities/RatingUsersListActivity.java
+++ app/src/main/java/com/BSLCommunity/CSN_student/Activities/RatingUsersListActivity.java
@@ -3,12 +3,10 @@ package com.BSLCommunity.CSN_student.Activities;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.widget.Button;
 import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.BSLCommunity.CSN_student.Objects.AnotherUserList;
 import com.BSLCommunity.CSN_student.R;
 
 public class RatingUsersListActivity extends AppCompatActivity {
@@ -25,10 +23,10 @@ public class RatingUsersListActivity extends AppCompatActivity {
     protected void drawUsersList() {
         LinearLayout listLL = findViewById(R.id.activity_rating_users_list_ll_list_users);
 
-        for (int i = 0; i < AnotherUserList.users.size(); ++i) {
+        for (int i = 0; i < 20; ++i) {
             LinearLayout userLayout = (LinearLayout) LayoutInflater.from(getApplicationContext()).inflate(R.layout.inflate_preview_user, listLL, false);
-            Button bt = (Button) userLayout.getChildAt(0);
-            bt.setText(AnotherUserList.users.get(i).nickName);
+           // Button bt = (Button) userLayout.getChildAt(0);
+            //bt.setText(AnotherUserList.users.get(i).nickName);
             listLL.addView(userLayout);
         }
     }

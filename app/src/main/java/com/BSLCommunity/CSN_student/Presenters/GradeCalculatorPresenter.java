742cb69df33bc03759404a338fe9d5fc380b59fa
Falcofemoralis <araraunapappagallo@gmail.com>
2021-06-20 00:01:03 +0300

    Improved translations. Fixed bugs.

--- app/src/main/java/com/BSLCommunity/CSN_student/Presenters/GradeCalculatorPresenter.java
+++ app/src/main/java/com/BSLCommunity/CSN_student/Presenters/GradeCalculatorPresenter.java
@@ -15,6 +15,8 @@ public class GradeCalculatorPresenter {
     private final GradeCalculatorView gradeCalculatorView;
     private final SubjectModel subjectModel;
     private final double RATING_MULTIPLY = 0.9;
+    private final double REGULAR_GRANT = 4.0;
+    private final double HIGH_GRANT = 5.0;
 
     public GradeCalculatorPresenter(GradeCalculatorView gradeCalculatorView) {
         this.gradeCalculatorView = gradeCalculatorView;
@@ -39,49 +41,58 @@ public class GradeCalculatorPresenter {
 
     public void calculateResult(ArrayList<String> marks) {
         int sum100 = 0, sum5 = 0, count = 0;
+        boolean isGrant = true;
 
-        for (String markText : marks) {
-            if (!markText.equals("")) {
-                int mark = Integer.parseInt(markText);
-                if (mark > 100) {
+        for (String mark : marks) {
+            if (!mark.equals("")) {
+                int mark100 = Integer.parseInt(mark);
+
+                int mark5;
+                try {
+                    mark5 = convert100ValueTo5(mark100);
+                } catch (Exception e) {
+                    e.printStackTrace();
                     gradeCalculatorView.showMsg(MarkErrorType.MORE100);
+                    reset();
                     return;
                 }
 
-                if (mark >= 60) {
-                    try {
-                        sum5 += convert100ValueTo5(mark);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-
-                    sum100 += mark;
+                if (mark100 >= 60) {
+                    sum100 += mark100;
+                    sum5 += mark5;
                     count++;
+
+                    if (mark5 == 3) {
+                        isGrant = false;
+                    }
                 } else {
-                    gradeCalculatorView.showResult(0, 0);
                     gradeCalculatorView.showMsg(MarkErrorType.EXAM_FAILED);
+                    reset();
                     return;
                 }
             }
 
-            if (count == 0) {
-                gradeCalculatorView.showResult(0, 0);
-                gradeCalculatorView.setGrant(GrantType.NO_GRANT);
-            } else {
+            if (count > 0) {
                 float result100 = (float) sum100 / count;
                 float result5 = (float) sum5 / count;
                 gradeCalculatorView.showResult((float) (result100 * RATING_MULTIPLY), result5);
 
-                if (result5 < 4.0) {
+                if (!isGrant || result5 < REGULAR_GRANT) {
                     gradeCalculatorView.setGrant(GrantType.NO_GRANT);
-                } else if (result5 < 5) {
+                } else if (result5 < HIGH_GRANT) {
                     gradeCalculatorView.setGrant(GrantType.REGULAR_GRANT);
                 } else {
                     gradeCalculatorView.setGrant(GrantType.HIGH_GRANT);
                 }
-
+            } else {
+                reset();
             }
         }
+    }
+
+    public void reset() {
+        gradeCalculatorView.showResult(0, 0);
+        gradeCalculatorView.setGrant(GrantType.NO_GRANT);
     }
 
     public int convert100ValueTo5(float value) throws Exception {
